b34f0c1f274872e3302bc0430751fff559d7b9a5
Nandini Yarra <yarranandhini1416@gmail.com>
2022-02-02 07:55:40 +0530

    REFACTORED:for json and csv file operation

--- src/main/java/com/bridgelabz/AddressBookManagement/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBookManagement/AddressBook.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import com.google.gson.Gson;
+import com.google.json.Json;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.StatefulBeanToCsv;
@@ -47,10 +47,14 @@ public class AddressBook implements AddressBookIF {
 	public ArrayList<ContactPerson> getContact() {
 		return new ArrayList<ContactPerson>(contactList.values());
 	}
+
+
 	@Override
 	public void operation() {
+
 		boolean moreChanges = true;
 		do {
+
 			System.out.println("\nChoose the operation you want to perform");
 			System.out.println(
 					"1.Add To Address Book\n2.Edit Existing Entry\n3.Delete Contact\n4.Display Address book\n5.Display Sorted Address Book By Custom Criteria\n6.Write To File\n7.Read Form File\n8.Write Data To CSV File\n9.Read Data From CSV File\n10.Write Data To JSON\n11.Read Data From JSON\n12.Exit Address book System");
@@ -59,27 +63,34 @@ public class AddressBook implements AddressBookIF {
 			case 1:
 				createContactPerson(scannerObject);
 				break;
+				
 			case 2:
 				editPerson();
 				break;
+				
 			case 3:
 				deletePerson();
 				break;
+				
 			case 4:
 				writeToAddressBookFile(IOService.CONSOLE_IO);
 				break;
+				
 			case 5:
 				System.out.println("What Criteria Do You Want Address Book To Be Sorted In ?");
 				System.out.println("1.FirstName\n2.City\n3.State\n4.Zip Code");
 				int sortingChoice = scannerObject.nextInt();
 				sortAddressBook(sortingChoice);
 				break;
+				
 			case 6: 
 				writeToAddressBookFile(IOService.FILE_IO);
 				break;
+				
 			case 7:
 				readDataFromFile(IOService.FILE_IO);
 				break;
+				
 			case 8:
 				try {
                     writeDataToCSV();
@@ -87,6 +98,7 @@ public class AddressBook implements AddressBookIF {
                     e.printStackTrace();
                 }
 				break;
+				
 			case 9:
 				try {
                     readDataFromCSV();
@@ -94,6 +106,7 @@ public class AddressBook implements AddressBookIF {
                     e.printStackTrace();
                 }
                 break;
+                
 			case 10:
 				try {
 					writeDataToJson();
@@ -120,6 +133,7 @@ public class AddressBook implements AddressBookIF {
 
 		} while (moreChanges);
 	}
+	
 	@Override
 	public void createContactPerson(Scanner scannerObject) {
 		
@@ -174,7 +188,9 @@ public class AddressBook implements AddressBookIF {
 	public void addContact(String firstName, ContactPerson person) {
 		
 			contactList.put(firstName.toLowerCase(), person);
+
 	}
+	
 	@Override	
 	public void addPersonToCity(ContactPerson contact) {
 		if (personByCity.containsKey(contact.getCity())) {
@@ -186,6 +202,7 @@ public class AddressBook implements AddressBookIF {
 			personByCity.put(contact.getCity(), cityList);
 		}
 	}
+	
 	@Override
 	public void addPersonToState(ContactPerson contact) {
 		if (personByState.containsKey(contact.getState())) {			
@@ -197,13 +214,18 @@ public class AddressBook implements AddressBookIF {
 			personByState.put(contact.getState(), stateList);
 		}
 	}
+	
 	@Override
 	public void editPerson() {
+		
 		ContactPerson person = new ContactPerson();
+
 		System.out.println("Enter the first name:");
 		String firstName = scannerObject.next();
+		
 		if(contactList.containsKey(firstName)) {
 			person = contactList.get(firstName);
+			
 			System.out.println("\nChoose the attribute you want to change:");
 			System.out.println("1.Last Name\n2.Phone Number\n3.Email\n4.City\n5.State\n6.ZipCode");
 			int choice = scannerObject.nextInt();
@@ -244,9 +266,13 @@ public class AddressBook implements AddressBookIF {
 		else {
 			System.out.println("Book Does Not Exist");
 		}
+		
+		
 	}
+
 	@Override
 	public void deletePerson() {
+
 		System.out.println("Enter the first name of the person to be deleted");
 		String firstName = scannerObject.next();
 		if(contactList.containsKey(firstName)) {
@@ -256,26 +282,32 @@ public class AddressBook implements AddressBookIF {
 		else {
 			System.out.println("Contact Not Found!");
 		}
+		
 	}
+
 	@Override
 	public void displayContents() {
-		System.out.println("----- Contents of the Address Book "+this.getAddressBookName()+" -----");
+		
+		System.out.println("Contents of the Address Book "+this.getAddressBookName()+" --");
 		for (String eachContact : contactList.keySet()) {
 			ContactPerson person = contactList.get(eachContact);
 			System.out.println(person);
 		}
-		System.out.println("-----------------------------------------");
+		System.out.println("---");
+
 	}
+	
 	@Override
 	public void printSortedList(List<ContactPerson> sortedContactList) {
-		System.out.println("------ Sorted Address Book "+this.getAddressBookName()+" ------");
+		System.out.println("Sorted Address Book "+this.getAddressBookName()+" --");
 		Iterator iterator = sortedContactList.iterator();
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 			System.out.println();
 		}
-		System.out.println("--");
+		System.out.println("---");
 	}
+	
 	@Override
 	public void sortAddressBook(int sortingChoice) {
 		List<ContactPerson> sortedContactList;
@@ -287,61 +319,77 @@ public class AddressBook implements AddressBookIF {
 					.collect(Collectors.toList());
 					printSortedList(sortedContactList);
 					break;
+				
 			case 2: sortedContactList = contactList.values().stream()
 					.sorted((firstperson, secondperson) -> firstperson.getCity().compareTo(secondperson.getCity()))
 					.collect(Collectors.toList());
 					printSortedList(sortedContactList);
 					break;
+				
 			case 3: sortedContactList = contactList.values().stream()
 					.sorted((firstperson, secondperson) -> firstperson.getState().compareTo(secondperson.getState()))
 					.collect(Collectors.toList());
 					printSortedList(sortedContactList);
 					break;
+				
 			case 4: sortedContactList = contactList.values().stream()
 					.sorted((firstperson, secondperson) -> Long.valueOf(firstperson.getZip()).compareTo(Long.valueOf(secondperson.getZip())))
 					.collect(Collectors.toList());
 					printSortedList(sortedContactList);
 					break;
 		}
+				
 	}
+	
 	@Override
 	public void writeToAddressBookFile(IOService ioService) {
 		if(ioService.equals(IOService.CONSOLE_IO))
 			displayContents();
+		
 		else if(ioService.equals(IOService.FILE_IO)) {
 			String bookName = this.getAddressBookName();
 			String fileName = bookName+".txt";
 			new AddressBookFileIO().writeToAddressBookFile(fileName, contactList);
 		}
 	}
+	
 	public void printData(IOService fileIo) {
 		String bookName = this.getAddressBookName();
 		String fileName = bookName+".txt";
 		if(fileIo.equals(IOService.FILE_IO)) new AddressBookFileIO().printData(fileName);
 	}
+
+
 	public long countEntries(IOService fileIo) {
+		
 		String bookName = this.getAddressBookName();
 		String fileName = bookName+".txt";
 		if(fileIo.equals(IOService.FILE_IO)) 
 			return new AddressBookFileIO().countEntries(fileName);
+		
 		return 0;
 	}
+	
 	@Override
 	public List<String> readDataFromFile(IOService fileIo) {
+		
 		List<String> employeePayrollFromFile = new ArrayList<String>();
 		if(fileIo.equals(IOService.FILE_IO)) {
 			System.out.println("Employee Details from payroll-file.txt");
 			String bookName = this.getAddressBookName();
 			String fileName = bookName+".txt";
 			employeePayrollFromFile = new AddressBookFileIO().readDataFromFile(fileName);
+			
 		}
 		return employeePayrollFromFile;
 	}
+	
 	@Override
 	public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
 		
 		String fileName = "./src\\test\\resources\\"+this.getAddressBookName()+"Contacts.csv";
         try (Writer writer = Files.newBufferedWriter(Paths.get(fileName));) {
+        	
             StatefulBeanToCsvBuilder<ContactPerson> builder = new StatefulBeanToCsvBuilder<>(writer);
             StatefulBeanToCsv<ContactPerson> beanWriter = builder.build();
             ArrayList<ContactPerson> listOfContacts= contactList.values().stream().collect(Collectors.toCollection(ArrayList::new));
@@ -353,12 +401,14 @@ public class AddressBook implements AddressBookIF {
             e.printStackTrace();
         }
     }
+	
 	@Override
     public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException {
     	
     	String fileName = "./src\\test\\resources\\"+this.getAddressBookName()+"Contacts.csv";
         try (Reader reader = Files.newBufferedReader(Paths.get(fileName));
              CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();){
+        	
             String[] nextRecord;
             while ((nextRecord = csvReader.readNext()) != null) {
                 System.out.println("First Name = " + nextRecord[2]);
@@ -372,22 +422,27 @@ public class AddressBook implements AddressBookIF {
             }
         }
     }
+    
 	@Override
 	public void writeDataToJson() throws IOException {
 		
 		String fileName = "./src\\test\\resources\\" + this.getAddressBookName() + "Contacts.json";
 		Path filePath = Paths.get(fileName);
-		Gson gson = new Gson();
+		Json gson = new Json();
 		String json = gson.toJson(contactList.values());
 		FileWriter writer = new FileWriter(String.valueOf(filePath));
 		writer.write(json);
 		writer.close();
+
 	}
+
 	@Override
     public void readDataFromJson() throws IOException {
+    	
         ArrayList<ContactPerson> contactList;
         String fileName = "./src\\test\\resources\\"+this.getAddressBookName()+"Contacts.json";
         Path filePath = Paths.get(fileName);
+        
         try (Reader reader = Files.newBufferedReader(filePath)) {
             Gson gson = new Gson();
             contactList = new ArrayList<>(Arrays.asList(gson.fromJson(reader, ContactPerson[].class)));
@@ -404,4 +459,5 @@ public class AddressBook implements AddressBookIF {
             }
         }
     }
+
 }
